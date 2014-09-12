3cd8dacd6c43b004e4d874683e20b1584e2a7913
Ustun Ozgur <ustun@sussman-2.local>
2014-09-12 01:55:18 -0700

    Karabiner, java etc.

--- manifests/site.pp
+++ manifests/site.pp
@@ -56,23 +56,22 @@ node default {
   include dnsmasq
   include git
   include hub
+include osx::global::key_repeat_rate
+include osx::dock::position
+  include karabiner #::login_item
+karabiner::remap{ 'optionR2controlL': }
   include nginx
+  include java
 
   # fail if FDE is not enabled
   if $::root_encrypted == 'no' {
-    fail('Please enable full disk encryption and try again')
+    #fail('Please enable full disk encryption and try again')
   }
 
   # node versions
-  include nodejs::v0_6
-  include nodejs::v0_8
   include nodejs::v0_10
 
   # default ruby versions
-  ruby::version { '1.9.3': }
-  ruby::version { '2.0.0': }
-  ruby::version { '2.1.0': }
-  ruby::version { '2.1.1': }
   ruby::version { '2.1.2': }
 
   # common, useful packages
